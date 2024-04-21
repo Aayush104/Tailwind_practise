69ce99ba14324dc5ed82441d2a9787ae07476923
Aayush Adhikari <aayushadhikari601@gmail.com>
2024-04-21 14:52:18 +0545

    Hero Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-        "primary": '#2a69e3'
+        "primary": '#2a69e3',
+      
+      },
+      fontSize: {
+        'xs': '0.75rem',    
+        'sm': '0.875rem',   
+        'base': '1rem',
+        'bt':'1.1rem',       
+        'lg': '1.500rem',  
+        'xl': '2.5rem',    
+        '2xl': '4.5rem',    
+        '3xl': '1.875rem',  
+        
+      },
+      width:{
+        'xs': '4rem',
+        'lg': "25rem"
       }
     },
   },
