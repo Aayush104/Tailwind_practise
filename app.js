87c268b6a6a4378b0b948307f91cbf7cf587e75e
Aayush Adhikari <aayushadhikari601@gmail.com>
2024-06-01 21:54:18 +0545

    Initial commit

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const nodemailer = require('nodemailer');
 const cors = require('cors');
 
+app.set("view engine", 'ejs')
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
