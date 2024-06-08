1282d8f7e1bcc54f712fb03b1d7a333ae9103459
Aayush Adhikari <aayushadhikari601@gmail.com>
2024-06-08 13:36:23 +0545

    Final commit

--- script.js
+++ script.js
@@ -15,17 +15,23 @@ document.addEventListener('click', (e) => {
 
     if (targetClassList.contains('nav_project') || targetClassList.contains('foot_project')) {
         scrollToSection(document.querySelector('.Project'));
-        handleMenu(document.getElementById('project'));
+        handleMenu();
     } else if (targetClassList.contains('nav_about') || targetClassList.contains('foot_about')) {
         scrollToSection(document.querySelector('.About'));
-        handleMenu(document.getElementById('about'));
+        handleMenu();
     } else if (targetClassList.contains('nav_home') || targetClassList.contains('foot_home')) {
         scrollToSection(document.querySelector('.Home'));
-        handleMenu(document.getElementById('home'));
-    } else if (targetClassList.contains('nav_contact') || targetClassList.contains('foot_contact') || targetClassList.contains('btn_contact')) {
+        handleMenu();
+    } else if (targetClassList.contains('nav_contact') || targetClassList.contains('foot_contact')) {
         scrollToSection(document.querySelector('.Contact'));
-        handleMenu(document.getElementById('contact'));
-        handleMenu(document.getElementById('hire'));
+      
+        handleMenu();
+        
+    }
+    else if (targetClassList.contains('btn_contact')) {
+        scrollToSection(document.querySelector('.Contact'));
+   
+        
     }
 });
 
